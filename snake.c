4da037ea0ef3eaf1deb848046edadcc10ee4f52a
Traksa <traksa7@gmail.com>
2018-05-28 20:33:03 +0200

    rysowanie planszy

--- snake.c
+++ snake.c
@@ -2,8 +2,7 @@
 #include<string.h>
 #define x 20
 #define y 20
-void plansza()
-{
+void plansza(){
 	int plansza[x][y];
 	int i,j;
 	int x1,y1;
@@ -15,10 +14,23 @@ void plansza()
 		}
 	}
 }
+void rysowanieplanszy(){
+	int i,j;
+	int x1,y1;
+	for(i=0;i<=y1+1;i++){
+		if(i==0)
+			printf("%c",35 );
+		else if(i==y1+1)
+			printf("%c",35);
+		else 
+			printf("%c",61);
+	}
+	
+}
 int main()
 {
 	plansza();
-	
+	rysowanieplanszy();
 }
 
 
