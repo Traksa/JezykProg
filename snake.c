88b420f4731e244089f052577b2c2e2a857e4737
Traksa <traksa7@gmail.com>
2018-05-29 20:16:23 +0200

    rysuje juz obramowke (problem ostatnim elementem po prawej stronie)

--- snake.c
+++ snake.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<string.h>
 #define x 20
-#define y 20
+#define y 40
 void plansza(){
 	int plansza[x][y];
 	int i,j;
@@ -15,22 +15,41 @@ void plansza(){
 	}
 }
 void rysowanieplanszy(){
+	int plansza[x][y];
 	int i,j;
-	int x1,y1;
-	for(i=0;i<=y1+1;i++){
+	for(i=0;i<=y+1;i++){
 		if(i==0)
 			printf("%c",35 );
-		else if(i==y1+1)
+		else if(i==y+1)
 			printf("%c",35);
 		else 
 			printf("%c",61);
 	}
-	
+	printf("\n");
+	for(i=0;i<x;i++){
+		printf("%c",124);
+
+		for(j=0;j<y;j++){
+		if(plansza[i][j] == 0)
+			printf(" ");
+		if(j==y-1)
+			printf("%c\n",124);
+		}	
+	}
+	for(i=0;i<=y+1;i++){
+	if(i==0)
+		printf("%c",35);
+	else if(i==y+1)
+		printf("%c",35);
+	else 
+		printf("%c",61);
+	}
 }
 int main()
 {
 	plansza();
 	rysowanieplanszy();
+	return 0;
 }
 
 
