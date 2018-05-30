c31134260b629e66351cd69c3b59e5624f29e782
Traksa <traksa7@gmail.com>
2018-05-30 15:38:12 +0200

    zmiana do maina losowanie owocow

--- snake.c
+++ snake.c
@@ -1,11 +1,16 @@
 #include<stdio.h>
 #include<string.h>
-#define x 20
-#define y 40
-void plansza(){
-	int plansza[x][y];
-	int i,j;
-	int x1,y1;
+#define x 22
+#define y 77
+
+int main()
+{
+int plansza[x][y];
+int i,j;
+int x1,y1;
+int xw,yw;
+int owoc,glowa;
+void Snake(){
 	for(i=0;i<x;i++)
 	{
 		for(j=0;j<y;j++)
@@ -13,10 +18,17 @@ void plansza(){
 			plansza[i][j]=0;
 		}
 	}
+	owoc=0;
+}
+void losowanieowocow(){
+	x1=1+rand()%2;
+	y1=1+rand()%3;
+	if(owoc == 0 && plansza[x1][y1]){
+		plansza[x1][y1]=-1;
+		owoc=1;
+	}
 }
 void rysowanieplanszy(){
-	int plansza[x][y];
-	int i,j;
 	for(i=0;i<=y+1;i++){
 		if(i==0)
 			printf("%c",35 );
@@ -30,10 +42,12 @@ void rysowanieplanszy(){
 		printf("%c",124);
 
 		for(j=0;j<y;j++){
-		if(plansza[i][j] == 0)
-			printf(" ");
-		if(j==y-1)
-			printf("%c\n",124);
+			if(plansza[i][j] == 0)
+				printf(" ");		
+			if(plansza[i][j] == -1)
+				printf("%c", 42);
+			if(j==y-1)
+				printf("%c\n",124);
 		}	
 	}
 	for(i=0;i<=y+1;i++){
@@ -45,11 +59,8 @@ void rysowanieplanszy(){
 		printf("%c",61);
 	}
 }
-int main()
-{
-	plansza();
-	rysowanieplanszy();
-	return 0;
+Snake();
+rysowanieplanszy();
+losowanieowocow();	
+
 }
-
-
