5b1fedddd6ed9267c97964f15ac84675db80c0ee
Traksa <traksa7@gmail.com>
2018-06-02 17:16:02 +0200

    dodawanie głowy

--- snake.c
+++ snake.c
@@ -1,15 +1,31 @@
 #include<stdio.h>
 #include<string.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <windows.h>
+#include <ctype.h>
+#include <time.h>
 #define x 22
 #define y 77
+#define glowa 5
+#define predkosc 80
 void Snake(int plansza[x][y],int owoc){
 	int i,j;
+	int x1,y1;
+	int gy1;
 	for(i=0;i<x;i++)
 	{
 		for(j=0;j<y;j++)
 		{
 			plansza[i][j]=0;
 		}
+	}
+	x1=x/2; y1=y/2;
+	owoc=0;
+	gy1=y1;
+	for(i=0;i<glowa;i++){
+		gy1++;
+		plansza[x1][gy1-glowa]=i+1;
 	}
 }
 void losowanieowocow(int plansza[x][y],int owoc){
@@ -40,6 +56,10 @@ void rysowanieplanszy(int plansza[x][y]){
 				printf(" ");		
 			if(plansza[i][j] == -1)
 				printf("%c", 42);
+			if(plansza[i][j]>0 && plansza[i][j] !=glowa)
+				printf("%c",91);
+			if(plansza[i][j]==glowa)
+				printf("%c",62);
 			if(j==y-1)
 				printf("%c\n",124);
 		}	
